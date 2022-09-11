27d399fea4b4ea953ade705b44e16cbbbfd73ba2
Pedro <65515149+pdroaq@users.noreply.github.com>
2022-09-10 23:47:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const mainContainer = document.querySelector(".container")
+const thanksContainer = document.querySelector(".thank-you")
+const submitButton = document.getElementById("submit")
+const rating = document.getElementById("rating")
+const rates = document.querySelectorAll(".btn")
+
+
+submitButton.addEventListener("click", () => {
+    thanksContainer.classList.remove("hidden")
+    mainContainer.style.display = "none"
+})
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML
+    })
+})
